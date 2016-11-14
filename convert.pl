8c991026606e56153e69413b8613e2f5e8920f8b
Jared-Lim <jaredlgw@outlook.com>
2016-11-14 16:15:26 +1100

    added some comments and check for output folder

--- convert.pl
+++ convert.pl
@@ -1,12 +1,25 @@
 #!/usr/bin/perl -w
 
+if(!"output/"){
+	print "WAH\n";
+}else{
+	print "wey\n";
+}
+
 opendir(DIR,"demons/");
 @tribes= grep { $_ ne '.' && $_ ne '..' } readdir(DIR);
+closedir(DIR);
 foreach(@tribes){
-	if(-e "output/".$_){
-		print "exists\t";
+	$tribe = $_;
+	#------------------------------------------
+	#checks if folder is created, if not create
+	#------------------------------------------
+	if(-e "output/".$tribe){
+		print $tribe."\n---tribe exists\n";
 	}else{
-		print "nope\t";
+		mkdir "output/".$tribe;
+		print "created ".$tribe."\n";
 	}
-	print $_."\n";
+	#------------------------------------------
+
 }
