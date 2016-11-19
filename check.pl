7ab28f31e0e841ebae3c13e34252439ccd0b5989
Jared-Lim <jaredlgw@outlook.com>
2016-11-19 16:24:04 +1100

    siphon part one

--- check.pl
+++ check.pl
@@ -1,3 +1,28 @@
 #!/usr/bin/perl -w
 
-print "Hello?";
+use Time::HiRes qw(usleep nanosleep);
+
+opendir(DIR,'output');
+@tribes= grep { $_ ne '.' && $_ ne '..'} readdir(DIR);
+closedir(DIR);
+
+foreach(@tribes){
+	$tribe = $_;
+	print $tribe."\n";
+	opendir(DIR,'output/'.$tribe);
+	@demons = grep { $_ ne '.' && $_ ne '..'} readdir(DIR);
+	closedir(DIR);
+	foreach(@demons){
+		$demon = $_;
+		print "---".$demon."\n";
+		open(FILE,'output/'.$tribe.'/'.$demon);
+		while($line = <FILE>){
+			if($line =~ /^\t\"(.*)\"/){
+				print $1."\n";
+			}
+		}
+		close FILE;
+		print "\n";
+		usleep(1000000);
+	}
+}
